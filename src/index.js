3cde45fea9b62aa02c48260e0f421ea1bd4d180d
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-02 19:04:00 +0100

    creation de la logique de routage et verification de sont bon fonctionnement

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './index.css';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Lodging from "./pages/Lodging";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Error from "./components/Error";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <Header />
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/aboutus">
+        <AboutUs />
+      </Route>
+      <Route path="/lodging">
+        <Lodging />
+      </Route>
+      <Route path="*">
+        <Error />
+      </Route>
+    </Router>
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
