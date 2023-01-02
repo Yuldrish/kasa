4dd2b1fb377060e8f75774cdf952730385f80773
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-02 17:59:24 +0100

    ajout de l'inmpiort de react router dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
