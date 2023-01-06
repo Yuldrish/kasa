1d684ff17e96539f62109a21ed7df6dc6be6b389
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-06 11:56:05 +0100

    creation de la page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,33 @@
+import BannerImg from "../../assets/banner.png";
+import Banner from "../../components/Banner";
+import Card from "../../components/card";
+import { Link } from "react-router-dom";
+import useFetch from "../../utils/hooks/useFetch";
+
 export default function Home() {
+
+  const {datas, isLoading} = useFetch("../logements.json")
+
   return(
-    <div>
-      <h1>bienvenue sur la page d'acceuil</h1>
+    <div className="home">
+      <Banner
+      text="Chez vous, partout et ailleurs"
+      picture={BannerImg}
+      />
+      {isLoading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="home__card">
+          {datas.map((Lodging) => (
+            <Link to={`/logements/${Lodging.id}`} key={Lodging.id}>
+              <Card 
+              title={Lodging.title}
+              picture={Lodging.picture[0]}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
