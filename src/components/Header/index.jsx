c13dcbc5cff48c3f9fb363821051ed73efc2a90e
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-06 08:34:16 +0100

    ajout du classe header__logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     //affichage
     return (
         <header>
-            <img src={logo} alt="logo_kasa" />
+            <img src={logo} alt="logo_kasa" className="header__logo" />
             <nav>
                 <Link to="/">Acceuil</Link>
                 <Link to="/À-propos-de-nous">À propos</Link>
