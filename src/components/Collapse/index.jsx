e8f1f9f96212248ee82ba046984fc46767b9c8fd
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-06 12:48:25 +0100

    creation du composant collapse

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
+export default function Collapse({label, description}) {
+    const [isHidden, setIsHidden] = useState(true)
+
+    return (
+        <div className="collapse">
+            <div className="collapse__header">
+                <div className="collapse__header__label">{label}</div>
+                {isHidden ?
+                (<div onClick={() => setIsHidden(false)} className="collapse__header__icon"><FaAngleDown /></div>)
+                :
+                (<div onClick={() => setIsHidden(true)} className="collapse__header__icon"><FaAngleUp /></div>)}
+            </div>
+            {!isHidden && <div className="collapse__description">{description}</div>}
+        </div>
+    )
+}
