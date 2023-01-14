810e463c0cc11683a2f6b5c2e41fca71e21b7ba8
Dimitri Raveleau <yuldrish@gmail.com>
2023-01-14 10:37:41 +0100

    modification de l'import des icons

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Collapse({label, description}) {
     const [isHidden, setIsHidden] = useState(true)
@@ -9,9 +10,9 @@ export default function Collapse({label, description}) {
             <div className="collapse__header">
                 <div className="collapse__header__label">{label}</div>
                 {isHidden ?
-                (<div onClick={() => setIsHidden(false)} className="collapse__header__icon"><FaAngleDown /></div>)
+                (<div onClick={() => setIsHidden(false)} className="collapse__header__icon"><FontAwesomeIcon icon={faAngleDown} /></div>)
                 :
-                (<div onClick={() => setIsHidden(true)} className="collapse__header__icon"><FaAngleUp /></div>)}
+                (<div onClick={() => setIsHidden(true)} className="collapse__header__icon"><FontAwesomeIcon icon={faAngleUp} /></div>)}
             </div>
             {!isHidden && <div className="collapse__description">{description}</div>}
         </div>
